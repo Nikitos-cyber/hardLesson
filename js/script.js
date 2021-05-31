940fd0ab0af22f4dfec9b74d459e156dd39dcfc4
Nikitos-cyber <golomazdin1212@gmail.com>
2021-05-31 20:40:49 +0300

    homework lesson_3 hard

--- js/script.js
+++ js/script.js
@@ -1,29 +1,131 @@
- 
-  let num = 266219;
-  let output = " ";
-  let result = 1;
-  function getLenght(num){
-  "use strict";
-   return num.toString().length;
-  }
-  
-  for( let i = 0; i < getLenght(num); i++){
-    
-    let str = num.toString(); 
-    output += str[i];
-    if ( i < getLenght(num) - 1)
-    {
-      output+=   ', ';
-    }
-    let int = str[i];
-    int = Number(int);
-    result *= int;
-  }
-  console.log(output);
-  console.log('result: ' + result);
-  
-  console.log('result в 3й степени: ' + result ** 3);
+ 'use strict';
+  let lang = ' ';
+  let year , month , day;
 
-  let string = String(num);
+  lang = prompt('Ru / En ?');
+
+ 
+ // Решение через is else
+  function getWeekDayRu(date){
+
+    let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+    return days[date.getDate()];
+  }
+
+
+  function getWeekDayEn(date){
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDate()];
+  }
+
   
-  console.log('Две первых цифры числа ' + string.substr(0, 2));
+
+  let date ;
+
+  if(lang === 'ru' || lang === 'Ru' || lang === 'RU' ){
+
+    alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    year = prompt('Введите текущий год:');
+    month = prompt('Введите текущий месяц:');
+    day = prompt('Введите текущий день:');
+
+    date = new Date(year, month ,day);
+
+    alert('Сегодня: ' +  getWeekDayRu(date));
+    
+
+  }else if(lang === 'en' || lang === 'En' || lang === 'EN'){
+    alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+    year = prompt('Enter the current year:');
+    month = prompt('Enter the current month:');
+    day = prompt('Enter the current day:');
+
+    date = new Date(year, month ,day);
+
+    alert(getWeekDayEn(date));
+  }else {
+    alert('This language is not supported!');
+  }
+  
+  // Решение через switch
+
+  lang = prompt('Ru / En ?');
+  switch(lang){
+
+    case 'ru':  
+      alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+      break;
+
+    case 'RU':  
+    alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    break;
+
+    case 'Ru':
+      alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+      break; 
+
+    case 'En':
+      alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+      break;
+      
+    case 'EN':
+      alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+      break;
+
+    case 'en':
+      alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+      break;
+
+    default: alert('This language is not supported!');
+
+  }
+
+  // Решение через Массив
+  
+  let array = [ ['Воскресенье','Понедельник' , 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' ],['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']];
+  
+  let ru =' ';
+
+   lang = prompt('ru / en ?');
+ 
+   function printRu(){
+      for (let i = 0; i < 7 ; i++){
+      ru += String(array[0][i]) + ' ';
+     
+      
+    }
+    alert('Дни недели:' + ru);
+
+   }
+
+   let result1 = lang === 'ru' ? printRu() : 0 ;
+
+   function printEn(){
+      for (let i = 0; i < 7 ; i++){
+      ru += String(array[1][i]) + ' ';
+     
+      
+    }
+    alert('Days of the week:' + ru);
+
+   }
+
+   let result2 = lang === 'en' ? printEn() : 0 ;
+
+
+   // задание 2
+   let namePerson ;
+
+   namePerson = prompt('Введите имя:');
+
+   let resulNameDirector = namePerson === 'Артем' ? console.log('Директор') : console.log('Студент');
+
+   let resulNameTeacher = namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
+
+
+ 
+    
+   
+  
+  
