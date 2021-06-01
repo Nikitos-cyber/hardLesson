0aabc0ab7e323a587a2f4329bc88e58865cc3d08
Nikitos-cyber <golomazdin1212@gmail.com>
2021-06-01 17:42:49 +0300

    hommework lesson_4 hard

--- js/script.js
+++ js/script.js
@@ -1,131 +1,28 @@
- 'use strict';
-  let lang = ' ';
-  let year , month , day;
+  'use strict';
 
-  lang = prompt('Ru / En ?');
-
- 
- // Решение через is else
-  function getWeekDayRu(date){
-
-    let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-
-    return days[date.getDate()];
-  }
-
-
-  function getWeekDayEn(date){
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return days[date.getDate()];
-  }
-
+  let str = '                                                         Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. На берегу, речью от всех? Большой, безорфографичный! Предупреждал ipsum взгляд но домах языкового ему запятой большой толку инициал продолжил заманивший напоивший, всеми большого живет всемогущая которой выйти ее, даль, это на берегу над назад рыбного которое своего. Своего себя повстречался языком несколько? Заманивший. ';
   
-
-  let date ;
-
-  if(lang === 'ru' || lang === 'Ru' || lang === 'RU' ){
-
-    alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    year = prompt('Введите текущий год:');
-    month = prompt('Введите текущий месяц:');
-    day = prompt('Введите текущий день:');
-
-    date = new Date(year, month ,day);
-
-    alert('Сегодня: ' +  getWeekDayRu(date));
-    
-
-  }else if(lang === 'en' || lang === 'En' || lang === 'EN'){
-    alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-    year = prompt('Enter the current year:');
-    month = prompt('Enter the current month:');
-    day = prompt('Enter the current day:');
-
-    date = new Date(year, month ,day);
-
-    alert(getWeekDayEn(date));
-  }else {
-    alert('This language is not supported!');
-  }
-  
-  // Решение через switch
-
-  lang = prompt('Ru / En ?');
-  switch(lang){
-
-    case 'ru':  
-      alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-      break;
-
-    case 'RU':  
-    alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break;
-
-    case 'Ru':
-      alert('Дни недели:  Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-      break; 
-
-    case 'En':
-      alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-      break;
-      
-    case 'EN':
-      alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-      break;
-
-    case 'en':
-      alert('Days of the week:  Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-      break;
-
-    default: alert('This language is not supported!');
-
-  }
-
-  // Решение через Массив
-  
-  let array = [ ['Воскресенье','Понедельник' , 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' ],['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']];
-  
-  let ru =' ';
-
-   lang = prompt('ru / en ?');
- 
-   function printRu(){
-      for (let i = 0; i < 7 ; i++){
-      ru += String(array[0][i]) + ' ';
+  let strEdit = function(string){
      
-      
+    if (typeof string != 'string'){
+      return ('Передана не строка!!');
     }
-    alert('Дни недели:' + ru);
-
-   }
-
-   let result1 = lang === 'ru' ? printRu() : 0 ;
-
-   function printEn(){
-      for (let i = 0; i < 7 ; i++){
-      ru += String(array[1][i]) + ' ';
-     
-      
-    }
-    alert('Days of the week:' + ru);
-
-   }
-
-   let result2 = lang === 'en' ? printEn() : 0 ;
 
 
-   // задание 2
-   let namePerson ;
+    let str = string.replace(/^\s+|\s+$/g, '');
 
-   namePerson = prompt('Введите имя:');
+    if(str.length > 30){
+      let text='';
 
-   let resulNameDirector = namePerson === 'Артем' ? console.log('Директор') : console.log('Студент');
+         for( let i = 30 ; i < str.length; i++){
+             
+           text += str[i];
+         }
 
-   let resulNameTeacher = namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
+        let result = str.replace(text ,'...');
+        return result;
+       }
+  };
 
-
- 
-    
-   
   
-  
+  console.log(strEdit(str));
